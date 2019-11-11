9a175e3bff2289da220432004f8a841d8aed62eb
eric <wangzan18@126.com>
2019-11-11 14:58:47 +0800

    update

--- configs/download-images.sh
+++ configs/download-images.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-docker pull registry.cn-hangzhou.aliyuncs.com/liuyi01/calico-node:v3.1.3
-docker tag registry.cn-hangzhou.aliyuncs.com/liuyi01/calico-node:v3.1.3 quay.io/calico/node:v3.1.3
-docker rmi registry.cn-hangzhou.aliyuncs.com/liuyi01/calico-node:v3.1.3
+docker pull calico/node:v3.10.1-2-g1d542f3-amd64
+docker tag calico/node:v3.10.1-2-g1d542f3-amd64 calico/node:v3.10.1
+docker rmi calico/node:v3.10.1-2-g1d542f3-amd64
 
-docker pull registry.cn-hangzhou.aliyuncs.com/liuyi01/calico-cni:v3.1.3
-docker tag registry.cn-hangzhou.aliyuncs.com/liuyi01/calico-cni:v3.1.3 quay.io/calico/cni:v3.1.3
-docker rmi registry.cn-hangzhou.aliyuncs.com/liuyi01/calico-cni:v3.1.3
+docker pull calico/cni:v3.10.1-2-gf2a76e2-amd64
+docker tag calico/cni:v3.10.1-2-gf2a76e2-amd64 calico/cni:v3.10.1
+docker rmi calico/cni:v3.10.1-2-gf2a76e2-amd64
+
+docker pull calico/typha:v3.10.1-2-g492ca4c-amd64
+docker tag calico/typha:v3.10.1-2-g492ca4c-amd64 calico/typha:v3.10.1
+docker rmi calico/typha:v3.10.1-2-g492ca4c-amd64
+
+
 
 docker pull registry.cn-hangzhou.aliyuncs.com/liuyi01/pause-amd64:3.1
 docker tag registry.cn-hangzhou.aliyuncs.com/liuyi01/pause-amd64:3.1 k8s.gcr.io/pause-amd64:3.1
 docker rmi registry.cn-hangzhou.aliyuncs.com/liuyi01/pause-amd64:3.1
 
-docker pull registry.cn-hangzhou.aliyuncs.com/liuyi01/calico-typha:v0.7.4
-docker tag registry.cn-hangzhou.aliyuncs.com/liuyi01/calico-typha:v0.7.4 quay.io/calico/typha:v0.7.4
-docker rmi registry.cn-hangzhou.aliyuncs.com/liuyi01/calico-typha:v0.7.4
-
-docker pull registry.cn-hangzhou.aliyuncs.com/liuyi01/coredns:1.1.3
-docker tag registry.cn-hangzhou.aliyuncs.com/liuyi01/coredns:1.1.3 k8s.gcr.io/coredns:1.1.3
-docker rmi registry.cn-hangzhou.aliyuncs.com/liuyi01/coredns:1.1.3
+docker pull coredns/coredns:1.6.4
+docker tag coredns/coredns:1.6.4 k8s.gcr.io/coredns:1.6.4
+docker rmi coredns/coredns:1.6.4
 
 docker pull registry.cn-hangzhou.aliyuncs.com/liuyi01/kubernetes-dashboard-amd64:v1.8.3
 docker tag registry.cn-hangzhou.aliyuncs.com/liuyi01/kubernetes-dashboard-amd64:v1.8.3 k8s.gcr.io/kubernetes-dashboard-amd64:v1.8.3
